f04e4beb0ef8aee6f1eee54c2f7a6f8437dbe0ce
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-04-19 20:13:11 +0300

    done

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -12,7 +12,21 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    if (size_ > 0) {
+      Node* temp_prev = nullptr;
+      Node* temp = front_;
+      Node* temp_next = temp->next;
+      while (temp_next != nullptr) {
+        temp->next = temp_prev;
+        temp_prev = temp;
+        temp = temp_next;
+        temp_next = temp_next->next;
+      }
+      temp->next = temp_prev;
+      Node* front = front_;
+      front_ = back_;
+      back_ = front;
+    }
   }
 
   /**
@@ -23,12 +37,23 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    if (size_ > 0) {
+      reverse_recursive_helper(front_->next, front_);
+      Node* temp = nullptr;
+      front_->next = temp;
+      Node* front = front_;
+      front_ = back_;
+      back_ = front;
+    }
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr == nullptr) {
+      return;
+    }
+    reverse_recursive_helper(curr->next, curr);
+    curr->next = prev;
   }
 
 }  // namespace assignment
